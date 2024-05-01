9d28225ea8a7c61adac8d89474d7a58415c81072
Victor <435victorjavier@gmail.com>
2024-04-30 18:27:00 -0600

    Crear dashboard

---
+++ app/App.tsx
@@ -0,0 +1,63 @@
+
+import { buttonVariants } from '@/components/ui/button'
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip'
+import { cn } from '@/lib/utils'
+import { Book, Users, CirclePlus, CircleCheck, LucideIcon } from 'lucide-react'
+import { Link, Outlet } from 'react-router-dom'
+import lensLogo from './assets/lens_logo.svg'
+
+interface NavProps {
+  links: {
+    title: string,
+    icon: LucideIcon,
+    location: string,
+  }[]
+}
+
+function Dashboard() {
+  const links = [
+    { title: 'Courses', icon: Book, location: 'courses' },
+    { title: 'Groups', icon: Users, location: 'groups' },
+    { title: 'Student Submissions', icon: CircleCheck, location: 'submissions' },
+    { title: 'Create Assignment', icon: CirclePlus, location: 'newAssignment' }
+  ]
+  return <TooltipProvider>
+    <div className="flex">
+      <MainNav links={links} />
+      <Outlet/>
+    </div>
+  </TooltipProvider>
+}
+
+function MainNav(props: NavProps) {
+  return (
+    <div
+      className="group flex flex-col gap-4 py-2 w-14 bg-secondary h-screen"
+    >
+      <img src={lensLogo} alt="Lens Logo" className="w-12 h-12 p-1 mb-6 mx-auto" />
+      <nav className="grid gap-2 px-2 justify-center">
+        {
+          props.links.map((link, index) => (
+            <Tooltip key={index} delayDuration={1200}>
+              <TooltipTrigger asChild>
+                <Link to={link.location} className={cn(
+                  buttonVariants({ variant: 'secondary', size: 'icon' }),
+                  "h-10 w-14"
+                )}>
+                  <link.icon className="h-5 w-5"></link.icon>
+                  <span className="sr-only">{link.title}</span>
+                </Link>
+              </TooltipTrigger>
+              <TooltipContent side='right'>
+                {link.title}
+              </TooltipContent>
+            </Tooltip>
+          ))
+        }
+      </nav>
+    </div>
+    
+  )
+}
+
+export default Dashboard
---
+++ app/main.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Dashboard from './App'
+import './globals.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <BrowserRouter basename='/app'>
+      <Routes>
+        <Route element={<Dashboard/>}>
+          <Route path='courses' element={<div>Courses</div>} />
+          <Route path='groups' element={<div>Groups</div>} />
+          <Route path='submissions' element={<div>Submissions</div>} />
+          <Route path='newAssignment' element={<div>New Assignment</div>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
+)
