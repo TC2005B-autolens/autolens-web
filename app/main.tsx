c7b95b5a25e85e7f2c07859c890da9f81c48346f
Victor <435victorjavier@gmail.com>
2024-05-01 14:33:15 -0600

    Vista de cursos

--- app/main.tsx
+++ app/main.tsx
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Dashboard from './App'
 import './globals.css'
+import CourseScreen from './routes/courses'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter basename='/app'>
       <Routes>
         <Route element={<Dashboard/>}>
-          <Route path='courses' element={<div>Courses</div>} />
+          <Route path='courses' element={<CourseScreen/>} />
           <Route path='groups' element={<div>Groups</div>} />
           <Route path='submissions' element={<div>Submissions</div>} />
           <Route path='newAssignment' element={<div>New Assignment</div>} />
---
+++ app/routes/courses.tsx
@@ -0,0 +1,46 @@
+import { cn } from "@/lib/utils"
+import { Users } from "lucide-react"
+
+interface CourseCardProps {
+  title: string,
+  groups: {
+    code: string,
+    members: number
+  }[]
+}
+
+const CourseCard = ({ title, groups }: CourseCardProps) => {
+  return (
+    <div className="outline outline-1 outline-slate-600 p-6 min-w-72 max-w-96">
+      <h2 className="text-2xl font-extrabold mb-3">{title}</h2>
+      <ul className="outline outline-1 outline-zinc-400 rounded-sm flex flex-col relative">
+        { groups.map((group, index) => (
+          <div className={cn(
+            "px-2 hover:bg-muted",
+            index !== 0 ? "border-t border-zinc-400" : ""
+          )}>
+            <li key={index} className="flex items-center gap-4 my-auto h-9 text-sm">
+              <span className="text-base">{group.code}</span>
+              <span className="ml-auto">{group.members}<Users className="inline w-[1rem] h-[1rem] ml-[0.2rem] mb-[0.2rem] text-muted-foreground"/></span>
+            </li>
+          </div>
+        )) }
+      </ul>
+    </div>
+  )
+}
+
+function CourseScreen() {
+  return (
+    <div>
+      <CourseCard title='Math' groups={[
+        { code: 'MATH101', members: 10 },
+        { code: 'MATH102', members: 12 },
+        { code: 'MATH103', members: 8 }
+      ]} />
+      <CourseCard title='Physics' groups={[{ code: 'PHYS101', members: 15 }]} />
+    </div>
+  )
+}
+
+export default CourseScreen;
