0c308a32489052a3edc35743f36752ee663a764e
rafarm04 <wafar0mo@hotmail.com>
2024-05-02 21:43:16 -0600

    endpoint para mostrar curso y sus grupos

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ client.connect((error) => {
   if (error) throw error;
   console.log("Conectada a la base de datos");
 });
-
+//muestra total de los miembroes de un grupo
 app.get("/member/:groupid/canva/nu", async (req, res) => {
   try {
     const { groupid } = req.params;
@@ -48,16 +48,17 @@ app.get("/member/:groupid/canva/nu", async (req, res) => {
     res.status(500).json({ error: "Error interno del servidor" });
   }
 });
-
+//Muestra los grupos en los que se encuentra un estudiante
 app.get("/asdnc/:studentid", async (req, res) => {
   try {
     const { studentid } = req.params;
     const query = `
       SELECT g.code AS group_code
-      FROM persons p
-      JOIN group_members gm ON p.id = gm.member_id
+      FROM group_members gm
       JOIN groups g ON gm.group_id = g.id
-      WHERE p.id = $1;
+      WHERE gm.member_id = $1
+      GROUP BY g.code;
+
     `;
     const result = await pool.query(query, [studentid]);
     res.json(result.rows);
@@ -66,95 +67,55 @@ app.get("/asdnc/:studentid", async (req, res) => {
     res.status(500).json({ error: "Error interno del servidor" });
   }
 });
+//Muestra todos los grupos que existen en la DB
+app.get("/groups/all", async (req, res) => {
+  try {
+    const query = `
+      SELECT code AS group_code
+      FROM groups;
+    `;
+    const result = await pool.query(query);
+    res.json(result.rows);
+  } catch (error) {
+    console.error("Error al ejecutar la consulta:", error.message);
+    res.status(500).json({ error: "Error interno del servidor" });
+  }
+});
+//Muestra todos los grupos que tiene un curso y los ensena en una lista
+app.get("/course/groups", async (req, res) => {
+  try {
+    const query = `
+      SELECT courses.id AS course_id, courses.title AS course_title, groups.code AS group_code
+      FROM groups
+      INNER JOIN courses ON groups.course_id = courses.id;
+    `;
+    const result = await client.query(query);
+    let courses = new Map();
 
-// app.get("/member/canva/:group_id/grading", async (req, res) => {
-//   try {
-//     const { group_id } = req.params;
-//     const query = `
-//       SELECT s.student_id AS ID,
-//             sub.assignment_id AS ID_Tarea,
-//             MAX(COALESCE(g.passed_tests, 0)) AS Pruebas
-//       FROM student s
-//       JOIN submission sub ON s.student_id = sub.student_id
-//       LEFT JOIN submission_analysis sa ON sub.id = sa.submission_id
-//       LEFT JOIN grading g ON sa.id = g.analysis_id
-//       WHERE s.canvas_group_id = $1
-//       GROUP BY s.student_id, sub.assignment_id;
-// `;
-//     res.json(rows);
-//   } catch (error) {
-//     console.error("Error al obtener los IDs de los estudiantes:", error);
-//     res.status(500).json({ error: "Error interno del servidor" });
-//   }
-// });
+    // Iterar sobre los resultados y agrupar los grupos por curso
+    result.rows.forEach((row) => {
+      const { course_id, course_title, group_code } = row;
 
-// app.post(
-//   "/canva/:assign_id/:course_id/:lang_id/:title/:description/:due_date/:group_id/publisher",
-//   async (req, res) => {
-//     try {
-//       const {
-//         assign_id,
-//         course_id,
-//         lang_id,
-//         title,
-//         description,
-//         due_date,
-//         group_id,
-//       } = req.params;
-
-//       const query1 =
-//         "SELECT canvas_group_id FROM student WHERE canvas_group_id = $1 LIMIT 1";
-//       const query2 =
-//         "INSERT INTO assignment (id, course_id, language_id, title, descript, due_date, pub_date) VALUES ($1, $2, $3, $4, $5, $6, CURRENT_DATE)";
-
-//       try {
-//         const result = await client.query(query1, [group_id]);
-//         if (result.rows.length === 0) {
-//           return res
-//             .status(400)
-//             .json({ error: "El grupo especificado no existe" });
-//         }
-
-//         await client.query(query2, [
-//           assign_id,
-//           course_id,
-//           lang_id,
-//           title,
-//           description,
-//           due_date,
-//         ]);
-
-//         res.status(201).json({ message: "Tarea creada exitosamente" });
-//       } catch (error) {
-//         console.error("Error al crear la tarea:", error);
-//         res.status(500).json({ error: "Error interno del servidor" });
-//       }
-//     } catch (error) {
-//       console.error("Error al procesar la solicitud:", error);
-//       res.status(500).json({ error: "Error interno del servidor" });
-//     }
-//   }
-// );
-
-// app.delete("/member/canva/:group_id/delete/:student_id", async (req, res) => {
-//   try {
-//     const { group_id, student_id } = req.params;
-//     const query =
-//       "DELETE FROM submission WHERE student_id='" +
-//       student_id +
-//       "' AND canvas_group_id = (SELECT canvas_id FROM teacher WHERE canvas_id = '" +
-//       group_id +
-//       "')";
-//     // Ejecutar la consulta SQL
-//     await client.query(query);
-//     res.status(200).json({
-//       message: `El estudiante ${student_id} ha sido eliminado del grupo ${group_id}`,
-//     });
-//   } catch (error) {
-//     console.error("Error al eliminar el estudiante del grupo:", error);
-//     res.status(500).json({ error: "Error interno del servidor" });
-//   }
-// });
+      // Verificar si ya existe el curso en el mapa
+      if (!courses.has(course_id)) {
+        // Si no existe, crear una nueva entrada para el curso
+        courses.set(course_id, {
+          course_id,
+          course_title,
+          groups: [group_code], // Inicializar la lista de grupos con el primer grupo
+        });
+      } else {
+        const course = courses.get(course_id);
+        course.groups.push(group_code);
+      }
+    });
+    const coursesArray = Array.from(courses.values());
+    res.json(coursesArray);
+  } catch (error) {
+    console.error("Error al ejecutar la consulta:", error.message);
+    res.status(500).json({ error: "Error interno del servidor" });
+  }
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
